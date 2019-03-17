39d737c11e6ab1fddb50e238bc9d4594fb2ac0c7
Charles Muchene <charlesmuchene@gmail.com>
2019-03-17 14:42:26 -0500

    Adds song

---
+++ src/main/java/dev/cstv/musify/domain/Album.java
@@ -0,0 +1,5 @@
+package dev.cstv.musify.domain;
+
+public class Album {
+
+}
---
+++ src/main/java/dev/cstv/musify/domain/Genre.java
@@ -0,0 +1,12 @@
+package dev.cstv.musify.domain;
+
+import java.util.List;
+
+public class Genre {
+    private List<Song> songs;
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+}
