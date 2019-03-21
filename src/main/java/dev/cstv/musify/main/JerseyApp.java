7601fbedb2cad7f2fa5121e7cb5f24cc9b23a6cb
Charles Muchene <charlesmuchene@gmail.com>
2019-03-20 19:12:50 -0500

    Adds Play resource

--- src/main/java/dev/cstv/musify/main/JerseyApp.java
+++ src/main/java/dev/cstv/musify/main/JerseyApp.java
@@ -1,6 +1,7 @@
 package dev.cstv.musify.main;
 
 import dev.cstv.musify.resources.DefaultResource;
+import dev.cstv.musify.resources.PlayResource;
 import dev.cstv.musify.resources.SongResource;
 import dev.cstv.musify.security.CORSResponseFilter;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -10,10 +11,13 @@ import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
  * Jersey application
  */
 public class JerseyApp extends ResourceConfig {
+
     public JerseyApp() {
         register(RequestContextFilter.class);
         register(DefaultResource.class);
         register(SongResource.class);
+        register(PlayResource.class);
         register(CORSResponseFilter.class);
     }
+
 }
--- src/main/java/dev/cstv/musify/resources/PlayResource.java
+++ src/main/java/dev/cstv/musify/resources/PlayResource.java
@@ -1,31 +1,24 @@
 package dev.cstv.musify.resources;
 
-import dev.cstv.musify.domain.Song;
 import dev.cstv.musify.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
 
-@Path("songs")
+@Path("/play")
 @Component
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class SongResource {
+public class PlayResource {
 
     @Autowired
     private SongService songService;
 
-    @GET
-    public List<Song> findAllSongs() {
-        return songService.findAll();
-    }
-
     @POST
-    public void save(Song song) {
-        songService.save(song);
+    @Path("/{id}")
+    public void play(@PathParam("id") Long id) {
+        songService.play(id);
     }
-
 }
